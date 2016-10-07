1e92af71108c3186a78426dde14989ebf8be4e2e
Luis Sala <albertitoclick@hotmail.com>
2016-10-07 01:46:33 -0400

    Register & Payment Activities Added

---
+++ MyApp2/app/src/main/java/com/example/gilbertojimenezorench/myapp/PaymentActivity.java
@@ -0,0 +1,14 @@
+package com.example.gilbertojimenezorench.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Secure Payment Info");
+        setContentView(R.layout.activity_payment);
+    }
+}
---
+++ MyApp2/app/src/main/java/com/example/gilbertojimenezorench/myapp/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.gilbertojimenezorench.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Please Register");
+        setContentView(R.layout.activity_register);
+    }
+}
