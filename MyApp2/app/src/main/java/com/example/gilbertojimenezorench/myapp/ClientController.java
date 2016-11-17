bbcb062dbf5d10e6e19527e479a194b97050f424
Luis Sala <albertitoclick@hotmail.com>
2016-11-17 18:31:52 -0400

    Modifies Manifest and Client Controller for testing purposes.

--- MyApp2/app/src/main/java/com/example/gilbertojimenezorench/myapp/ClientController.java
+++ MyApp2/app/src/main/java/com/example/gilbertojimenezorench/myapp/ClientController.java
@@ -1,66 +1,97 @@
 package com.example.gilbertojimenezorench.myapp;
 
-import android.os.StrictMode;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class ClientController {
-
-    public static ResultSet select(String user, String pass, String sqlCommand){
-        Connection c = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
+public class ClientController extends AppCompatActivity {
 
 
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://dbserver.capscbgbnhob.us-west-2.rds.amazonaws.com:5432/",
-                            user, pass);
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
-            rs = stmt.executeQuery( sqlCommand);
-            return rs;
-            //rs.close();
-            //stmt.close();
-            //c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-            return rs;
-        }
-        //System.out.println("Operation done successfully");
-        //return rs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new getdata().execute();
+
     }
 
-    public static boolean sqlCommand(String user, String pass, String sqlCommand){
-        Connection c = null;
-        Statement stmt = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://dbserver.capscbgbnhob.us-west-2.rds.amazonaws.com:5432/",
-                            user, pass);
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
-            String sql = sqlCommand;
-            stmt.executeUpdate(sql);
-            c.commit();
-            stmt.close();
-            c.close();
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-            return false;
+    /**
+     * Method to retrieve Information
+     */
+    public class getdata extends AsyncTask<Void, Integer, String> {
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = "";
+            System.out.println("Do background");
+            HttpURLConnection connection = null;
+            int statusCode = 0;
+
+            try {
+                URL url = new URL("http://morning-caverns-51343.herokuapp.com/db");
+
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestMethod("GET");
+
+                statusCode = connection.getResponseCode();
+                System.out.println("Async call code: " + connection.getResponseCode());
+
+                if (statusCode == 200) {
+                    System.out.println("Server responded with code: " + statusCode);
+
+                    InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line = "";
+                    String res = "";
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        res += line;
+                    }
+
+            /* Close Stream */
+                    if (null != inputStream) {
+                        inputStream.close();
+                    }
+
+                    System.out.println("Hello: " + res);
+                    // Code goes here
+
+                } else {
+                    System.out.println("error");
+                }
+
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                connection.disconnect();
+                System.out.println("disconnected");
+            }
+
+            return result;
         }
-        System.out.println("Operation done successfully");
-        return true;
-    }
 
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+
+            // progressDialog.setCancelable(true);
+            //  }
+        }
+
+    }
 
 }
